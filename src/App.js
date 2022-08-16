139be96610a5ff94c970771174a1f43631084e06
salsabilaathika-97 <salsathikar@gmail.com>
2022-08-16 22:59:48 +0800

    get data from api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, {Component} from "react";
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = { test: "hai", lists: [] }
   }
+
+  componentDidMount() {
+    axios.get(`https://reqres.in/api/users?page=2`)
+    .then(res => {
+      const lists = res.data.data;
+      this.setState({lists})
+    })
+  }
+
   state = {  }
   render() { 
     return ( 
-      <h1>Hai</h1>
+      <>
+        <h1>Hai</h1>
+        <h2>{this.state.test}</h2>
+        <ul>
+          {this.state.lists.map(list => <li>{list.first_name}</li>)}
+        </ul>
+      </>
      );
   }
 }
